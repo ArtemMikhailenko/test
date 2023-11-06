418f6a5c4b1e3328d1ed92a9970b54f37eb1dc6f
Artem_Mykhailenko <Artemlend@gmail.com>
2023-11-06 04:31:37 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,132 @@
+$('.qwer').hide();
+var flag = true;
+$('.btn_1').click(function(){
+  if(flag) {
+    $('.qwer').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer').slideUp(300);
+    flag=1;
+  }
+})
+$('.qwer2').hide();
+var flag = true;
+$('.btn_2').click(function(){
+  if(flag) {
+    $('.qwer2').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer2').slideUp(300);
+    flag=1;
+  }
+})
+$('.qwer3').hide();
+var flag = true;
+$('.btn_3').click(function(){
+  if(flag) {
+    $('.qwer3').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer3').slideUp(300);
+    flag=1;
+  }
+})
+$('.qwer4').hide();
+var flag = true;
+$('.btn_4').click(function(){
+  if(flag) {
+    $('.qwer4').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer4').slideUp(300);
+    flag=1;
+  }
+})
+$('.qwer5').hide();
+var flag = true;
+$('.btn_5').click(function(){
+  if(flag) {
+    $('.qwer5').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer5').slideUp(300);
+    flag=1;
+  }
+})
+$('.qwer6').hide();
+var flag = true;
+$('.btn_6').click(function(){
+  if(flag) {
+    $('.qwer6').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer6').slideUp(300);
+    flag=1;
+  }
+})
+$('.qwer7').hide();
+var flag = true;
+$('.btn_7').click(function(){
+  if(flag) {
+    $('.qwer7').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer7').slideUp(300);
+    flag=1;
+  }
+})
+$('.qwer8').hide();
+var flag = true;
+$('.btn_8').click(function(){
+  if(flag) {
+    $('.qwer8').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer8').slideUp(300);
+    flag=1;
+  }
+})
+$('.qwer9').hide();
+var flag = true;
+$('.btn_9').click(function(){
+  if(flag) {
+    $('.qwer9').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer9').slideUp(300);
+    flag=1;
+  }
+})
+$('.qwer10').hide();
+var flag = true;
+$('.btn_10').click(function(){
+  if(flag) {
+    $('.qwer10').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer10').slideUp(300);
+    flag=1;
+  }
+})
+$('.qwer11').hide();
+var flag = true;
+$('.btn_11').click(function(){
+  if(flag) {
+    $('.qwer11').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer11').slideUp(300);
+    flag=1;
+  }
+})
+$('.qwer12').hide();
+var flag = true;
+$('.btn_12').click(function(){
+  if(flag) {
+    $('.qwer12').slideDown(300);
+    flag =0;
+  } else {
+    $('.qwer12').slideUp(300);
+    flag=1;
+  }
+})
